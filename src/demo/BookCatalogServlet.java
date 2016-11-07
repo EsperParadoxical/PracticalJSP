3df4b210736eba2a526c8c202b7becede0f369bc
EsperParadoxical <onglianjie97@gmail.com>
2016-11-07 16:22:25 +0800

    Draft 1

---
+++ src/demo/BookCatalogServlet.java
@@ -0,0 +1,7 @@
+package demo;
+
+/**
+ * Created by 151260D on 11/7/2016.
+ */
+public class BookCatalogServlet {
+}
---
+++ src/demo/BookDetailsServlet.java
@@ -0,0 +1,8 @@
+package demo;
+
+/**
+ * Created by 151260D on 11/7/2016.
+ */
+public class BookDetailsServlet {
+
+}
